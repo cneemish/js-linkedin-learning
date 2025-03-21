08ec820761df191b0c098ca8147632c5b0ffeb15
cneemish <cneemish@gmail.com>
2025-03-21 14:17:18 +0530

    main

---
+++ conditions.js
@@ -0,0 +1,2 @@
+var answer = window.confirm("Are you sure? Press Yes or Press No");
+
---
+++ logical.js
@@ -0,0 +1,11 @@
+let animal1 = "Cat", animal2 = "Dog", animal3 = "Tiger";
+
+if (animal1 === "work" || animal2 === "ant") {
+    console.log("Animal")
+} else if (animal3 == "Tiger" && animal1 == "Cat") {
+    console.log ("They are related")
+} else {
+    console.log (animal2)
+}
+
+
