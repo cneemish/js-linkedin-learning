2ce8d80af762d3ada450aaf9d4d35e39dbc5b65d
cneemish <cneemish@gmail.com>
2025-03-22 00:00:37 +0530

    add

--- website/index.js
+++ website/index.js
@@ -4,5 +4,32 @@
    /*  window.alert(`this is an alert!`);
     window.alert(`Hi How are you?`); */
 
-document.getElementById("testOne").textContent = `Hellow`;
-document.getElementById("paragraphOne").textContent = `This is a paragraph`;
+/* document.getElementById("testOne").textContent = `Hellow`;
+document.getElementById("paragraphOne").textContent = `This is a paragraph`; */
+
+
+var answer = window.prompt("Type Yes, No, Maybe");
+/* if (answer === "Yes") {
+    console.log(`You typed Yes`);  
+} else if(answer ==="No") {
+    console.log(`You typed no`);
+} else if (answer === "Maybe"){
+    console.log('You Typed maybe');
+} else{
+    console.log('Something else?')
+} */
+
+switch (answer) {
+    case "Yes":
+        console.log(`You typed Yes`);
+        break;
+    case "No":
+        console.log(`You typed no`);
+        break;
+    case "Maybe":
+        console.log(`You typed Maybe`);
+        break;
+    default:
+        console.log('Something else?')
+        break;
+}
